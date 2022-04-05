bff6ac919218b2ecfd5e3c99ea7c26f33cb2a458
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-05 16:27:58 -0400

    Added employees data

--- src/data.js
+++ src/data.js
@@ -2,37 +2,58 @@ export default  [
     {
         name: "Spider-man",
         title: "Spider-like abilities",
-        img: "https://i.annihil.us/u/prod/marvel/i/mg/9/30/538cd33e15ab7/standard_xlarge.jpg"
+        img: "https://i.annihil.us/u/prod/marvel/i/mg/9/30/538cd33e15ab7/standard_xlarge.jpg",
+        officePhone: '678-123-0002',
+        mobilePhone: '678-123-4002',
+        email: 'spider-man@fakeemail.com'
     },
     {
         name: "Iron-man",
         title: "Superhuman strength",
-        img: "https://i.annihil.us/u/prod/marvel/i/mg/6/a0/55b6a25e654e6/standard_xlarge.jpg"
+        img: "https://i.annihil.us/u/prod/marvel/i/mg/6/a0/55b6a25e654e6/standard_xlarge.jpg",
+        officePhone: '678-456-9876',
+        mobilePhone: '678-432-1234',
+        email: 'ironman@fakeemail.com'
     },
     {
         name: "Black Panther",
         title: "Panther God",
-        img: "https://i.annihil.us/u/prod/marvel/i/mg/1/c0/537ba2bfd6bab/standard_xlarge.jpg"
+        img: "https://i.annihil.us/u/prod/marvel/i/mg/1/c0/537ba2bfd6bab/standard_xlarge.jpg",
+        officePhone: '678-553-3333',
+        mobilePhone: '678-000-4444',
+        email: 'panther@fakeemail.com'
     },
     {
         name: "Doctor Strange",
         title: "Possessing vast magical knowledge",
-        img: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/009drs_ons_crd_02.jpg"
+        img: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/009drs_ons_crd_02.jpg",
+        officePhone: '678-555-1111',
+        mobilePhone: '678-888-4444',
+        email: 'strange@fakeemail.com'
     },
     {
         name: "Captain America",
         title: "Captain Roger",
-        img: "https://i.annihil.us/u/prod/marvel/i/mg/3/50/537ba56d31087/standard_xlarge.jpg"
+        img: "https://i.annihil.us/u/prod/marvel/i/mg/3/50/537ba56d31087/standard_xlarge.jpg",
+        officePhone: '678-999-2222',
+        mobilePhone: '678-888-4444',
+        email: 'captain@fakeemail.com'
     },
     {
         name: "Wong",
         title: "Master of the Mystic Arts ",
-        img: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/079wng_ons_crd_03.jpg"
+        img: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/079wng_ons_crd_03.jpg",
+        officePhone: '678-234-9876',
+        mobilePhone: '678-123-3456',
+        email: 'wong@fakeemail.com'
     },
     {
         name: "Wanda Maximoff",
         title: "Notably powerful",
-        img: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/012scw_ons_crd_02.jpg"
+        img: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/012scw_ons_crd_02.jpg",
+        officePhone: '678-987-6543',
+        mobilePhone: '678-987-1234',
+        email: 'wanda@fakeemail.com'
     },
 ];
 
