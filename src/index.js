1bd42649c7676fc68c3554808fee8487cf67d422
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-05 16:35:26 -0400

    Imports StrictMode &  React router dom

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+import { StrictMode } from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './App.css';
+import { BrowserRouter as Route } from 'react-router-dom'
+
+
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <StrictMode>
+   <Route>
+     <App />
+   </Route>
+ </StrictMode>
   document.getElementById('root')
 );
 
